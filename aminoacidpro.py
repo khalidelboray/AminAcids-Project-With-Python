6a78079850b10872759df1e7ff75478cbebffe55
Khaled Mohamed Elborey <37024839+khalidelboray@users.noreply.github.com>
2020-03-05 19:55:58 +0200

    better name lookup using hash/dictionary
    this is the code i used to convert the two arrays into a proper Python structure
    https://gist.github.com/khalidelboray/7082a31026678105ef6461e8a9d9755f

--- aminoacidpro.py
+++ aminoacidpro.py
@@ -3,29 +3,15 @@ print("\t Hello in Amino acid program \t")
 print("*************************************************************")
 
 
-aminoname=input("Enter the name of amino acid:")
-aminoname=aminoname.upper()
-aminoacids=["TTT","TTC","TTA","TTG","TCT","TCC","TCA","TCG","TAT","TAC","TAA"
-,"TAG","TGT","TGC","TGA","TGG","CTT","CTC","CTA","CTG","CCT","CCC","CCA","CCG","CAT"
-,"CAC","CAA","CAG","CGT","CGC","CGA","CGG","ATT","ATC","ATA","ATG","ACT","ACC","ACA"
-,"ACG","AAT","AAC","AAA","AAG","AGT","AGC","AGA","AGG","GTT","GTC","GTA","GTG","GCT"
-,"GCC","GCA","GCG","GAT","GAC","GGT","GGC","GGA","GGG"]
-aminoacidsname=["Phe(F)","Phe(F)","Leu(L)","Leu(L)","Ser(S)","Ser(S)","Ser(S)","Ser(S)"
-,"Tyr(Y)","Tyr(Y)","Stop","Stop","Cys(C)","Cys(C)", "Stop", "Trp(W)" ,"leu(L)","leu(L)"
-,"leu(L)","leu(L)","Pro(P)","Pro(P)","Pro(P)","Pro(P)","His(H),His(H)","Gln(G)","Gln(G)"
-,"Arg(R)","Arg(R)","Arg(R)","Arg(R)","Ile(I)","Ile(I)","Ile(I)","Met(M)","Thr(T)","Thr(T)"
-,"Thr(T)","Thr(T)","Asn(N)","Asn(N)","Lys(K)","Lsy(K)","Ser(S)","Ser(S)","Ser(S)","Arg(R)"
-,"Arg(R)","Val(v)","Val(v)","Val(v)","Val(v)","Asp(D)","Asp(D)","Glu(E)","Glu(E)","Gly(G)"
-,"Gly(G)","Gly(G)","Gly(G)"]
-i=0
-find=False
-for X in aminoacids:
-    if(X==aminoname):
-        print("The amino acid you entered is:"+ aminoacidsname[i])    
-        find=True
-    i+=1        
-if (find==False):
-    print("Wrong Amino acid")    
+names = {"TTT": "Phe(F)", "TTC": "Phe(F)", "TTA": "Leu(L)", "TTG": "Leu(L)", "TCT": "Ser(S)", "TCC": "Ser(S)", "TCA": "Ser(S)", "TCG": "Ser(S)", "TAT": "Tyr(Y)", "TAC": "Tyr(Y)", "TAA": "Stop", "TAG": "Stop", "TGT": "Cys(C)", "TGC": "Cys(C)", "TGA": "Stop", "TGG": "Trp(W)", "CTT": "leu(L)", "CTC": "leu(L)", "CTA": "leu(L)", "CTG": "leu(L)", "CCT": "Pro(P)", "CCC": "Pro(P)", "CCA": "Pro(P)", "CCG": "Pro(P)", "CAT": "His(H),His(H)", "CAC": "Gln(G)", "CAA": "Gln(G)", "CAG": "Arg(R)", "CGT": "Arg(R)", "CGC": "Arg(R)", "CGA": "Arg(R)", "CGG": "Ile(I)", "ATT": "Ile(I)", "ATC": "Ile(I)", "ATA": "Met(M)", "ATG": "Thr(T)", "ACT": "Thr(T)", "ACC": "Thr(T)", "ACA": "Thr(T)", "ACG": "Asn(N)", "AAT": "Asn(N)", "AAC": "Lys(K)", "AAA": "Lsy(K)", "AAG": "Ser(S)", "AGT": "Ser(S)", "AGC": "Ser(S)", "AGA": "Arg(R)", "AGG": "Arg(R)", "GTT": "Val(v)", "GTC": "Val(v)", "GTA": "Val(v)", "GTG": "Val(v)", "GCT": "Asp(D)", "GCC": "Asp(D)", "GCA": "Glu(E)", "GCG": "Glu(E)", "GAT": "Gly(G)", "GAC": "Gly(G)", "GGT": "Gly(G)", "GGC": "Gly(G)", "GGA": None, "GGG": None}
+
+
+aminoname=input("Enter the name of amino acid: ")
+
+if names[aminoname.upper()] is not None :
+    print("The amino acid you entered is: [ {} ]".format(names[aminoname.upper()]))
+else:
+    print("Wrong Amino acid") 
     
 
  
